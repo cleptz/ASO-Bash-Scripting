b742942318f262cbe04afc5d35210d630a2ad530
Pablo Barroso Bastardo <pablobarrosobboy@hotmail.com>
2019-01-09 11:58:39 +0100

    Update tareas.sh

--- tareas.sh
+++ tareas.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+################
+## Author: Pablo Barroso Bastardo.
+## Description: script sencillo que realiza una serie de tareas básicas
+## en un determinado orden.
+################
+
 clear
 date
 cal
