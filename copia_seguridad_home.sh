b2d37a9e52481c1540ccda89ac2f70758e40925a
Pablo Barroso Bastardo <pablobarrosobboy@hotmail.com>
2019-01-09 11:56:35 +0100

    Update copia_seguridad_home.sh

--- copia_seguridad_home.sh
+++ copia_seguridad_home.sh
@@ -1,2 +1,8 @@
+###################
+## Author: Pablo Barroso Bastardo
+## Description: script para realizar una copia de un seguridad de un directorio en concreto.
+##
+###################
+
 #!/bin/bash
 tar -czf /tmp/myhome_directorio.tar.gz /home/pablo
